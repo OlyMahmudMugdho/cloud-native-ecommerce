6820b197fca293061be671536134eb6ce58411f2
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2025-07-04 07:06:17 +0000

    add cloud router and nat

--- infrastructure/cloud_router.tf
+++ infrastructure/cloud_router.tf
@@ -2,15 +2,4 @@ resource "google_compute_router" "workload_network_router" {
   name    = var.workload_network_router_name
   network = google_compute_network.workloads_network.id
   region  = var.region
-
-}
-
-
-resource "google_compute_router_nat" "workload_nat" {
-  project                            = var.project
-  name                               = var.workload_nat_name
-  router                             = google_compute_router.workload_network_router.name
-  region                             = var.region
-  nat_ip_allocate_option             = "AUTO_ONLY"
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
