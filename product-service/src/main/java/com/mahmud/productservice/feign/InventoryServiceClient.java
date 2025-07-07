5f637ef6429a695fd319589ce6b145bc8ec90667
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2025-07-07 11:18:15 +0000

    product-service: remove eureka, use spring load balancer

--- product-service/src/main/java/com/mahmud/productservice/feign/InventoryServiceClient.java
+++ product-service/src/main/java/com/mahmud/productservice/feign/InventoryServiceClient.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "INVENTORY-SERVICE")
+@FeignClient(name = "inventory-service")
 public interface InventoryServiceClient {
 
     @GetMapping("/inventory/api/products")
