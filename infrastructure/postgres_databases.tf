624f39961379952b8835f7d9434a1c97cd68273e
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2025-07-07 08:57:30 +0000

    add deletion policy

--- infrastructure/postgres_databases.tf
+++ infrastructure/postgres_databases.tf
@@ -1,14 +1,17 @@
 resource "google_sql_database" "order_db" {
   name     = var.order_db_name
   instance = google_sql_database_instance.postgres_instance.name
+  deletion_policy = "DELETE"
 }
 
 resource "google_sql_database" "cart_db" {
   name     = var.carts_db_name
   instance = google_sql_database_instance.postgres_instance.name
+  deletion_policy = "DELETE"
 }
 
 resource "google_sql_database" "auth_db" {
   name     = var.auth_db_name
   instance = google_sql_database_instance.postgres_instance.name
+  deletion_policy = "DELETE"
 }
