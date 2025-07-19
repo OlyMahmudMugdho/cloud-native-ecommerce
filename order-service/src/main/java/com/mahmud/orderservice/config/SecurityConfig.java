546d05230744846e9d7460d5603fe03353b70541
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2025-07-19 07:57:20 +0000

    update cors config

--- order-service/src/main/java/com/mahmud/orderservice/config/SecurityConfig.java
+++ order-service/src/main/java/com/mahmud/orderservice/config/SecurityConfig.java
@@ -9,6 +9,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,6 +29,14 @@ public class SecurityConfig {
                         .requestMatchers("/orders/webhook").permitAll() // Allow Stripe webhook
                         .anyRequest().authenticated()
                 )
+                .cors(cors -> cors.configurationSource(request -> {      // CORS configuration
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Arrays.asList("http://localhost:5173")); // Allow React origin
+                        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));          // Allowed methods
+                        config.setAllowedHeaders(Arrays.asList("Authorization", "content-type"));        // Allow Authorization header
+                        config.setAllowCredentials(false);
+                        return config;
+                    }))
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
                 );
