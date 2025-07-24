16451656449355f7c90b024850fb24d3ccf88fb5
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2025-07-24 07:51:56 +0000

    bug fix for mongodb install

--- ansible/scripts/deploy_mongodb_keycloak.sh
+++ ansible/scripts/deploy_mongodb_keycloak.sh
@@ -1,5 +1,5 @@
-GCP_PROJECT=$(grep 'project' ~/cloud-native-ecommerce/infrastructure/terraform.tfvars | awk -F' = ' '{print $2}' | tr -d '"') && \
-GCP_ZONE=$(grep 'zone' ~/cloud-native-ecommerce/infrastructure/terraform.tfvars | awk -F' = ' '{print $2}' | tr -d '"') && \
+GCP_PROJECT=$(grep 'project' ../..//infrastructure/terraform.tfvars | awk -F' = ' '{print $2}' | tr -d '"') && \
+GCP_ZONE=$(grep 'zone' ../..//infrastructure/terraform.tfvars | awk -F' = ' '{print $2}' | tr -d '"') && \
 gcloud compute ssh mongodb-keycloak-server --zone="${GCP_ZONE}" --command='bash -s' <<EOF
 sudo bash -c '
 POSTGRES_HOST="${POSTGRES_HOST}"
