25fa6833a778809f1e2951da7a43df3b8ebf9234
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2025-07-24 08:19:52 +0000

    destruction script updated

--- infrastructure/destroy.sh
+++ infrastructure/destroy.sh
@@ -3,10 +3,19 @@ GCP_PROJECT=$(grep 'project' terraform.tfvars | awk -F' = ' '{print $2}' | tr -d
 GCP_ZONE=$(grep 'zone' terraform.tfvars | awk -F' = ' '{print $2}' | tr -d '"') && \
 gcloud auth login --cred-file=account.json --quiet && \
 gcloud config set project ${GCP_PROJECT} --quiet
-gcloud compute  instances delete mongodb-keycloak-server --zone=$GCP_ZONE --quiet && \
-gcloud container clusters delete workload-cluster --zone=$GCP_ZONE && \
-# gcloud sql databases delete keycloak -i database-instance --quiet
-# gcloud sql databases delete order_db -i database-instance --quiet
-# gcloud sql databases delete cart_db -i database-instance --quiet
+gcloud compute  instances stop mongodb-keycloak-server --zone=$GCP_ZONE --quiet && \
+
+kubectl delete ing --all -n cloud-native-ecommerce
+kubectl delete pods --all -n cloud-native-ecommerce
+kubectl delete svc --all -n cloud-native-ecommerce
+kubectl delete secret --all -n cloud-native-ecommerce
+kubectl delete configmap --all -n cloud-native-ecommerce
+kubectl delete ns cloud-native-ecommerce
+
+gcloud sql databases delete keycloak -i database-instance --quiet
+gcloud sql databases delete order_db -i database-instance --quiet
+gcloud sql databases delete cart_db -i database-instance --quiet
+
+# gcloud container clusters delete workload-cluster --zone=$GCP_ZONE && \
 terraform plan && \
 terraform destroy
